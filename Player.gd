fc3cdc5ef7f472871d97107da2a70543e201f588
Manjul <manjultamrakar4@gmail.com>
2023-01-09 22:32:25 +0545

    added moving enemy, generic hitbox, custom resources,up next ladders and double jump

--- Player.gd
+++ Player.gd
@@ -1,12 +1,7 @@
 extends KinematicBody2D
+class_name Player
 
-export (int) var JUMP_FORCE = -130
-export (int) var JUMP_RELEASE_FORCE = - 70
-export (int) var MAX_SPEED = 50
-export (int) var ACCELERATION = 10
-export (int) var FRICTION = 10
-export (int) var GRAVITY = 4
-export (int) var FALL_GRAVITY = 4
+export (Resource) var moveData
 
 var velocity = Vector2.ZERO
 
@@ -27,21 +22,24 @@ func _physics_process(delta):
 	else:
 		apply_acceleration(input.x)
 		animatedSprite.animation = "run"
-		if input.x > 0:
-			animatedSprite.flip_h = true
-		elif input.x < 0:
-			animatedSprite.flip_h = false
+		
+		animatedSprite.flip_h = input.x > 0
+		
+		##if input.x > 0:
+			##animatedSprite.flip_h = true
+		##elif input.x < 0:
+			##animatedSprite.flip_h = false
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
-			velocity.y = JUMP_FORCE
+			velocity.y = moveData.JUMP_FORCE
 	else:
 		animatedSprite.animation = "jump"
 		if Input.is_action_just_released("ui_up") and velocity.y < -70:
-			velocity.y = JUMP_RELEASE_FORCE
+			velocity.y = moveData.JUMP_RELEASE_FORCE
 			
 		if velocity.y > 0:
-			velocity.y += FALL_GRAVITY
+			velocity.y += moveData.FALL_GRAVITY
 	
 	var was_in_air = not is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -51,11 +49,11 @@ func _physics_process(delta):
 		animatedSprite.frame = 1
 
 func apply_gravity():
-	velocity.y += GRAVITY
+	velocity.y += moveData.GRAVITY
 	velocity.y = min(velocity.y, 400)
 
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, FRICTION)
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
 
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
+	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
