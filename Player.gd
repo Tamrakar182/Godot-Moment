895cb5918646cf0aec856a12786650e27d477ac9
Manjul <manjultamrakar4@gmail.com>
2023-01-10 13:44:13 +0545

    added ladders, double jump, buffered jump and coyote jumps, up next looping path enemies

--- Player.gd
+++ Player.gd
@@ -1,52 +1,118 @@
 extends KinematicBody2D
 class_name Player
 
+enum { MOVE, CLIMB }
+
 export (Resource) var moveData
 
 var velocity = Vector2.ZERO
+var state = MOVE
+var double_jump = 1
+var buffered_jump = false
+var coyote_jump = false
 
-onready var animatedSprite = $AnimatedSprite
-
-func _ready():
-	animatedSprite.frames = load("res://player_green_skin.tres")
+onready var animatedSprite: = $AnimatedSprite
+onready var ladderCheck: = $LadderCheck
+onready var jumpBufferTimer: = $JumpBufferTimer
+onready var coyoteJumpTimer: = $CoyoteJumpTimer
 
 func _physics_process(delta):
-	apply_gravity()
 	var input = Vector2.ZERO
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.y = Input.get_axis("ui_up", "ui_down")
+
+	match state:
+		MOVE: move_state(input)
+		CLIMB: climb_state(input)
+
+func move_state(input):
+	if isOnLadder() and Input.is_action_pressed("ui_up"):
+		state = CLIMB
 	
-	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	
-	if input.x == 0:
+	apply_gravity()
+	if not horizontal_move(input):
 		apply_friction()
 		animatedSprite.animation = "idle"
 	else:
 		apply_acceleration(input.x)
 		animatedSprite.animation = "run"
-		
 		animatedSprite.flip_h = input.x > 0
-		
-		##if input.x > 0:
-			##animatedSprite.flip_h = true
-		##elif input.x < 0:
-			##animatedSprite.flip_h = false
 	
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
-			velocity.y = moveData.JUMP_FORCE
+		reset_double_jump()
 	else:
 		animatedSprite.animation = "jump"
-		if Input.is_action_just_released("ui_up") and velocity.y < -70:
-			velocity.y = moveData.JUMP_RELEASE_FORCE
-			
-		if velocity.y > 0:
-			velocity.y += moveData.FALL_GRAVITY
+	
+	if can_jump():
+		input_jump()
+	else:
+		input_jump_release()
+		input_double_jump()
+		buffer_jump()
+		fast_fall()
 	
 	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
 	var just_landed = is_on_floor() and was_in_air
 	if just_landed:
 		animatedSprite.animation = "run"
 		animatedSprite.frame = 1
+	
+	var just_left_ground = not is_on_floor() and was_on_floor
+	if just_left_ground and velocity.y >= 0:
+		coyote_jump = true
+		coyoteJumpTimer.start()
+
+func horizontal_move(input):
+	return input.x != 0
+
+func can_jump():
+	return is_on_floor() or coyote_jump
+
+func input_jump():
+	if Input.is_action_just_pressed("ui_up") or buffered_jump:
+		velocity.y = moveData.JUMP_FORCE
+		buffered_jump = false
+
+func input_jump_release():
+	if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
+		velocity.y = moveData.JUMP_RELEASE_FORCE
+
+func input_double_jump():
+	if Input.is_action_just_pressed("ui_up") and double_jump > 0:
+		velocity.y = moveData.JUMP_FORCE
+		double_jump -= 1
+
+func fast_fall():
+	if velocity.y > 0:
+		velocity.y += moveData.FALL_GRAVITY
+
+func buffer_jump():
+	if Input.is_action_just_pressed("ui_up"):
+		buffered_jump = true
+		jumpBufferTimer.start()
+
+func reset_double_jump():
+	double_jump = moveData.DOUBLE_JUMP_COUNT
+
+func climb_state(input):
+	if not isOnLadder():
+		state = MOVE
+	if input.length() != 0:
+		animatedSprite.animation = "run"
+	else:
+		animatedSprite.animation = "idle"
+	velocity = input * moveData.CLIMB_SPEED
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func isOnLadder():
+	if not ladderCheck.is_colliding(): return false
+	var collider = ladderCheck.get_collider()
+	if not collider is Ladder: return false
+	return true
 
 func apply_gravity():
 	velocity.y += moveData.GRAVITY
@@ -57,3 +123,9 @@ func apply_friction():
 
 func apply_acceleration(amount):
 	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
+
+func _on_JumpBufferTimer_timeout():
+	buffered_jump = false
+
+func _on_CoyoteJumpTimer_timeout():
+	coyote_jump = false
